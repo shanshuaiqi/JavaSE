c9eb8e366bcfd2bbce69f19f60575f778d25264d
shanshuaiqi <34259256+shanshuaiqi@users.noreply.github.com>
2019-10-31 17:18:43 +0800

    增加前端样式

---
+++ src/main/java/com/example/demo/project/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.demo.project.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 类功能描述
+ *
+ * @author ssq
+ * @date 2019-10-31 上午 10:53
+ */
+@Controller
+public class AuthController {
+
+	@RequestMapping("/test")
+	public Object test() {
+		return "hello world";
+	}
+
+	@RequestMapping("/index")
+	public String toIndex(Model model) {
+		model.addAttribute("name", "lisi");
+		return "index";
+	}
+}
